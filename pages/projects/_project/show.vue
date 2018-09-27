b8d6284f902fdf877ef42b294506501cf22f302e
Parham Alvani <parham.alvani@gmail.com>
2018-09-28 02:08:13 +0330

    Adds thing create dialog.

--- pages/projects/_project/show.vue
+++ pages/projects/_project/show.vue
@@ -6,6 +6,31 @@
     <v-flex class="xs12 sm8 md6 pt-4">
       <i-thing v-for="(thing, i) in things" :key="i" :thing="thing" :project="projectID"></i-thing>
     </v-flex>
+    <v-flex>
+      <v-dialog v-model="dialog" persistent max-width="500px">
+        <v-btn slot="activator" color="primary" dark>Create Thing</v-btn>
+        <v-card>
+          <v-card-title>
+            <span class="headline">Project</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container grid-list-md>
+              <v-layout column>
+                <v-flex xs12 sm6 md4>
+                  <v-text-field v-model="name" label="Name" required></v-text-field>
+                </v-flex>
+              </v-layout>
+            </v-container>
+            <small>*indicates required field</small>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" flat @click.native="dialog = false">Close</v-btn>
+            <v-btn color="blue darken-1" flat @click.native="create">Create</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-flex>
   </v-layout>
 </template>
 
@@ -16,6 +41,35 @@ export default {
   components: {
     'i-thing': IThing
   },
+
+  methods: {
+    async refresh () {
+      try {
+        this.things = await this.$axios.$get(`http://192.168.73.5:1375/api/projects/${this.projectID}/things`)
+        console.log(this.things)
+      } catch (e) {
+        console.log(e)
+      }
+    },
+    async create () {
+      try {
+        await this.$axios.$post(`http://192.168.73.5:1375/api/projects/${this.projectID}/things`, {
+          'name': this.name,
+          'model': 'default'
+        })
+      } catch (e) {
+        console.log(e)
+      }
+      await this.refresh()
+      this.dialog = false
+    }
+  },
+
+  data: () => ({
+    dialog: false, // create thing dialog visibility
+
+    name: '' // name field in create thing
+  }),
 
   async asyncData ({app, params}) {
     let projectID = params.project

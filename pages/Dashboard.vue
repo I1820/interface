cdfdb926b8a3bb1f6997d72183e00ddf9f334799
Parham Alvani <parham.alvani@gmail.com>
2018-09-27 16:31:28 +0330

    Simpler dashboard is better ;)

--- pages/Dashboard.vue
+++ pages/Dashboard.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-layout column justify-center align-center>
+  <v-layout column justify-space-around align-center>
     <v-flex xs12 sm8 md6>
       <div class="text-xs-center">
         <img src="/i1820.png" alt="I1820" class="mb-5" style="width: 50%" />
@@ -20,31 +20,5 @@
         </v-card-text>
       </v-card>
     </v-flex>
-    <v-flex class="xs12 sm8 md6 pt-4">
-      <div>
-        <h2 class="white--text display-2">Projects</h2>
-      </div>
-      <i-project v-for="(project, i) in projects" :key="i" :project="project"></i-project>
-    </v-flex>
-  </v-layout>
+	</v-layout>
 </template>
-
-<script>
-import IProject from '~/components/i-project.vue'
-export default {
-  components: {
-    'i-project': IProject
-  },
-
-  async asyncData ({app}) {
-    let projects = []
-    try {
-      projects = await app.$axios.$get('http://192.168.73.5:1375/api/projects')
-      console.log(projects)
-    } catch (e) {
-      console.log(e)
-    }
-    return {projects}
-  }
-}
-</script>

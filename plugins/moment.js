1ff14e21fbb5e8ffbfb4159cc0e52df5c6d485e1
Parham Alvani <parham.alvani@gmail.com>
2018-09-27 16:30:44 +0330

    Adds moment plugin configuration.

---
+++ plugins/moment.js
@@ -0,0 +1,15 @@
+/*
+ *
+ * In The Name of God
+ *
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 27-09-2018
+ * |
+ * | File Name:     moment.js
+ * +===============================================
+ */
+import Vue from 'vue'
+
+Vue.use(require('vue-moment'))

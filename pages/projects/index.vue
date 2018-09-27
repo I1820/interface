0a957ced517777a5a7afcf99600265c0efac5132
Parham Alvani <parham.alvani@gmail.com>
2018-09-27 16:26:57 +0330

    Adds projects page.

---
+++ pages/projects/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-layout column justify-space-around align-center>
+    <v-flex class="xs12 sm8 md6 pt-4">
+      <i-project v-for="(project, i) in projects" :key="i" :project="project"></i-project>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import IProject from '~/components/i-project.vue'
+export default {
+  components: {
+    'i-project': IProject
+  },
+
+  async asyncData ({app}) {
+    let projects = []
+    try {
+      projects = await app.$axios.$get('http://192.168.73.5:1375/api/projects')
+      console.log(projects)
+    } catch (e) {
+      console.log(e)
+    }
+    return {projects}
+  }
+}
+</script>

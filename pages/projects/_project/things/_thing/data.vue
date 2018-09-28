190eca0667d209c693d0a7fafa53fd7e59cbb914
Parham Alvani <parham.alvani@gmail.com>
2018-09-28 09:53:07 +0330

    Fetch data with button like a charm :joy:

--- pages/projects/_project/things/_thing/data.vue
+++ pages/projects/_project/things/_thing/data.vue
@@ -5,16 +5,18 @@
     </v-flex>
     <v-flex>
       <template v-for="(info, name) in assets">
-        <v-subheader inset>{{name}}</v-subheader>
-				<v-data-table
-					:key="name"
-					:headers="headers"
+        <v-subheader inset>{{name}} a.k.a {{info.title}}</v-subheader>
+        <v-btn :key="`${name}-btn`" @click="recently(name)">Fetch {{info.title}}</v-btn>
+        <v-data-table
+          :key="`${name}-data-table`"
+          :headers="headers"
           :items="states[name]"
+          item-key="at"
           hide-actions
           class="elevation-1"
-        >
-					<template slot="items" slot-scope="props">
-          	<td>{{ props.item.raw }}</td>
+          >
+          <template slot="items" slot-scope="props">
+            <td>{{ props.item.raw }}</td>
             <td>{{ props.item.at | moment("DD-MM-YYYY hh:mm:ss") }}</td>
           </template>
         </v-data-table>
@@ -26,6 +28,25 @@
 <script>
 
 export default {
+  methods: {
+    async recently (assetName) {
+      try {
+        this.$set(this.states, assetName, await this.$axios.$get(`http://192.168.73.5:1373/api/projects/${this.projectID}/things/${this.thingID}/assets/${assetName}/queries/recently?limit=10`))
+      } catch (e) {
+        console.log(e)
+      }
+      console.log(this.states[assetName])
+    }
+  },
+
+  computed: {
+    rows: {
+      get () {
+        return this.states
+      }
+    }
+  },
+
   async asyncData ({app, params}) {
     var projectID = params.project
     var thingID = params.thing
@@ -49,15 +70,6 @@ export default {
         sortable: true
       }
     ]
-    for (var asset in assets) {
-      try {
-        states[asset] = await app.$axios.$get(`http://192.168.73.5:1373/api/projects/${projectID}/things/${thingID}/assets/${asset}/queries/recently?limit=10`)
-      } catch (e) {
-        console.log(e)
-        states[asset] = []
-      }
-      console.log(states[asset])
-    }
     return {projectID, thingID, assets, states, headers}
   }
 }

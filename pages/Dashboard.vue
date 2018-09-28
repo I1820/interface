fdd79a2f88f2479f7fe5c51fd3986586df97f51f
Parham Alvani <parham.alvani@gmail.com>
2018-09-28 16:26:27 +0330

    Adds health check for I1820 components.

--- pages/Dashboard.vue
+++ pages/Dashboard.vue
@@ -20,5 +20,41 @@
         </v-card-text>
       </v-card>
     </v-flex>
-	</v-layout>
+    <v-flex>
+      <v-btn @click="checkPM" :color="isPMOnline ? 'green' : 'red'">check pm component</v-btn>
+      <v-btn @click="checkDM" :color="isDMOnline ? 'green' : 'red'">check dm component</v-btn>
+    </v-flex>
+  </v-layout>
 </template>
+
+<script>
+export default {
+  methods: {
+    async checkPM () {
+      try {
+        var about = await this.$axios.$get('/pm/about')
+        if (about === '18.20 is leaving us') {
+          this.isPMOnline = true
+        }
+      } catch (e) {
+        console.log(e)
+      }
+    },
+    async checkDM () {
+      try {
+        var about = await this.$axios.$get('/dm/about')
+        if (about === '18.20 is leaving us') {
+          this.isDMOnline = true
+        }
+      } catch (e) {
+        console.log(e)
+      }
+    }
+
+  },
+  data: () => ({
+    isPMOnline: false,
+    isDMOnline: false
+  })
+}
+</script>

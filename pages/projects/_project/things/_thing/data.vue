fc86ee5179fee695468135fa797470e15f5abdb7
Parham Alvani <parham.alvani@gmail.com>
2018-09-28 10:02:10 +0330

    More beautiful data page.

--- pages/projects/_project/things/_thing/data.vue
+++ pages/projects/_project/things/_thing/data.vue
@@ -5,21 +5,29 @@
     </v-flex>
     <v-flex>
       <template v-for="(info, name) in assets">
-        <v-subheader inset>{{name}} a.k.a {{info.title}}</v-subheader>
-        <v-btn color="orange" :key="`${name}-btn`" @click="recently(name)">Fetch {{info.title}}</v-btn>
-        <v-data-table
-          :key="`${name}-data-table`"
-          :headers="headers"
-          :items="states[name]"
-          item-key="at"
-          hide-actions
-          class="elevation-1"
-          >
-          <template slot="items" slot-scope="props">
-            <td>{{ props.item.raw }}</td>
-            <td>{{ props.item.at | moment("DD-MM-YYYY hh:mm:ss") }}</td>
-          </template>
-        </v-data-table>
+        <v-layout column justify-center align-center>
+          <v-flex>
+            <v-subheader>{{name}} a.k.a {{info.title}}</v-subheader>
+          </v-flex>
+          <v-flex>
+            <v-btn color="orange" :key="`${name}-btn`" @click="recently(name)">Fetch {{info.title}}</v-btn>
+          </v-flex>
+          <v-flex>
+            <v-data-table
+              :key="`${name}-data-table`"
+              :headers="headers"
+              :items="states[name]"
+              item-key="at"
+              hide-actions
+              class="elevation-1"
+              >
+              <template slot="items" slot-scope="props">
+                <td>{{ props.item.raw }}</td>
+                <td>{{ props.item.at | moment("DD-MM-YYYY hh:mm:ss") }}</td>
+              </template>
+            </v-data-table>
+          </v-flex>
+        </v-layout>
       </template>
     </v-flex>
   </v-layout>

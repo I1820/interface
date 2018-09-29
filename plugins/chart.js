18d8ccdc478f4101c6e746f5f389c4717dcf8875
Parham Alvani <parham.alvani@gmail.com>
2018-09-29 10:30:42 +0000

    Adds chart plugin.

---
+++ plugins/chart.js
@@ -0,0 +1,16 @@
+/*
+ *
+ * In The Name of God
+ *
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 29-09-2018
+ * |
+ * | File Name:     chart.js
+ * +===============================================
+ */
+import Vue from 'vue'
+import HighchartsVue from 'highcharts-vue'
+
+Vue.use(HighchartsVue)

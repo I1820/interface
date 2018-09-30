9f2118053633ea94e751be0ff983b882f63b73e2
Parham Alvani <parham.alvani@gmail.com>
2018-09-30 19:28:35 +0330

    Completes chart page.

--- pages/projects/_project/things/_thing/chart.vue
+++ pages/projects/_project/things/_thing/chart.vue
@@ -8,6 +8,16 @@
         <v-container grid-list-md>
           <v-layout column>
             <v-flex xs12 sm6>
+              <v-checkbox label="Partial Fetch" v-model="isPfetch"></v-checkbox>
+            </v-flex>
+            <v-flex xs12 sm6>
+              <v-text-field
+                label="Window Size"
+                v-model="windowSize"
+                :disabled="!isPfetch"
+                ></v-text-field>
+            </v-flex>
+            <v-flex xs12 sm6>
               <v-select
                 :items="assets"
                 v-model="selectedAssets"
@@ -74,7 +84,7 @@
         </v-container>
       </v-card-text>
       <v-card-actions>
-        <v-btn color="blue darken-1" flat @click.native="fetch">Fetch</v-btn>
+        <v-btn color="blue darken-1" flat @click.native="isPfetch ? pfetch() : fetch">Fetch</v-btn>
       </v-card-actions>
     </v-card>
     <highcharts :options="chart"></highcharts>
@@ -85,6 +95,61 @@
 
 export default {
   methods: {
+    async pfetch () {
+      if (this.selectedAssets.length === 0) {
+        this.$toast.global.iError({message: 'Please select at least one asset'})
+        return
+      }
+      if (this.windowSize === 0) {
+        this.$toast.global.iError({message: 'Please specify window size'})
+        return
+      }
+      if (this.fromDate === '') {
+        this.$toast.global.iError({message: 'Please specify "FROM"'})
+        return
+      }
+      if (this.toDate === '') {
+        this.$toast.global.iError({message: 'Please specify "TO"'})
+        return
+      }
+
+      const series = []
+      for (const asset of this.selectedAssets) {
+        try {
+          const data = []
+          for (const bulk of await this.$axios.$post(
+            `dm/api/projects/${this.projectID}/things/${this.thingID}/queries/pfetch`,
+            {
+              target: asset,
+              window: {
+                size: parseInt(this.windowSize)
+              },
+              range: {
+                from: this.$moment(`${this.fromDate}T${this.fromTime}:00`).format(),
+                to: this.$moment(`${this.toDate}T${this.toTime}:00`).format()
+              }
+            })
+          ) {
+            data.push({
+              name: `${this.$moment(bulk.since).format('dddd, MMMM Do YYYY, h:mm:ss a')} - ${this.$moment(bulk.until).format('dddd, MMMM Do YYYY, h:mm:ss a')} - count: ${bulk.count}`,
+              x: (Date.parse(bulk.since) + Date.parse(bulk.until)) / 2,
+              y: bulk.data
+            })
+          }
+          series.push({
+            name: asset,
+            description: '', // TODO use asset title
+            data: data,
+            color: '#' + (Math.random() * 0xFFFFFF << 0).toString(16)
+          })
+        } catch (e) {
+          this.$toast.global.iError({message: `${e.response.data.code}: ${e.response.data.error.split('\n')[0]}`})
+        }
+      }
+
+      this.$set(this.chart, 'series', series)
+    },
+
     async fetch () {
       if (this.selectedAssets.length === 0) {
         this.$toast.global.iError({message: 'Please select at least one asset'})
@@ -109,8 +174,8 @@ export default {
               type: 'number',
               target: asset,
               range: {
-                from: Date.parse(`${this.fromDate}T${this.fromTime}:00`).toISOString(),
-                to: Date.parse(`${this.toDate}T${this.toTime}:00`).toISOString()
+                from: new Date(`${this.fromDate}T${this.fromTime}:00`).toISOString(),
+                to: new Date(`${this.toDate}T${this.toTime}:00`).toISOString()
               }
             })
           ) {
@@ -121,6 +186,7 @@ export default {
           }
           series.push({
             name: asset,
+            description: '', // TODO use asset title
             data: data,
             color: '#' + (Math.random() * 0xFFFFFF << 0).toString(16)
           })
@@ -139,6 +205,8 @@ export default {
     fromTime: '00:00', // hh-mm
     toDate: '', // hh-mm
     toTime: '00:00', // YY-mm-dd
+    windowSize: 0,
+    isPfetch: false,
 
     chart: {
       title: {

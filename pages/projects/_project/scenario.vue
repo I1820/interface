bbf90e7258ca8f4143592be10ac958619bf541cc
Parham Alvani <parham.alvani@gmail.com>
2018-10-07 05:44:53 +0330

    Adds editor for project scenario.

---
+++ pages/projects/_project/scenario.vue
@@ -0,0 +1,22 @@
+<template>
+  <ace-editor v-model="content" lang="python" theme="tomorrow_night" height="500" @init="initEditor"></ace-editor>
+</template>
+
+<script>
+
+export default {
+  data: () => ({
+    content: '# turn on the air conditioner when air temperature goes up'
+  }),
+
+  methods: {
+    initEditor (editor) {
+      require('brace/mode/python')
+      require('brace/theme/tomorrow_night')
+
+      editor.setShowPrintMargin(false)
+      editor.setShowInvisibles(true)
+    }
+  }
+}
+</script>

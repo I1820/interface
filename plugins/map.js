df550dd7d910950e47fcf732f981a2ea95ba6ac3
Parham Alvani <parham.alvani@gmail.com>
2018-10-13 13:55:09 +0330

    Adds google map plugin.

---
+++ plugins/map.js
@@ -0,0 +1,19 @@
+/*
+ *
+ * In The Name of God
+ *
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 13-10-2018
+ * |
+ * | File Name:     map.js
+ * +===============================================
+ */
+import Vue from 'vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
+
+Vue.use(VueGoogleMaps, {
+  key: '',
+  libraries: 'places'
+})

a0dd013f0073d0ae3324b70493d8dedaf3c14fa9
Parham Alvani <parham.alvani@gmail.com>
2018-10-19 05:39:17 +0330

    Adds very very simple index page.

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-layout column justify-space-around align-center>
+    <h2>Welcome to Avidnet platform management panel.</h2>
+    <p>
+      In order to use this panel you must to login with
+      <a href="https://auth0.com">Auth0</a>.<br>
+      If your internet connection is ok go to
+      <nuxt-link to="/login">login page</nuxt-link>
+      and login.
+    </p>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  auth: false,
+  layout: 'none',
+  mounted () {
+    if (this.$auth.loggedIn) {
+      this.$router.push('dashboard')
+    }
+  }
+}
+</script>
